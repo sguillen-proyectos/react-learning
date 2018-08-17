705feeb69824ed30e075bc754a845af033da0ca8
Sergio Guillen Mantilla <serguimant@gmail.com>
2018-08-16 23:03:00 -0400

    Test unscaping

--- misc/src/App.js
+++ misc/src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import Conditional from './Conditional';
 import './App.css';
 
+function Message({msg}) {
+  return <React.Fragment>
+    <span>{msg}</span>
+    <br />
+  </React.Fragment>
+}
+
+function Message2(props) {
+  return <span>{props.children}</span>;
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +20,16 @@ class App extends Component {
         <h1>Conditional Rendering</h1>
         <Conditional />
         <hr />
+        <h1>Expression evaluation</h1>
+        <Message msg="&lt; <" />
+        <Message msg={"&lt; <"} />
+        <br />
+        <Message2>
+          {"Some stuff &lt;3"}
+        </Message2>
+        <Message2>
+          Some stuff &lt;3
+        </Message2>
       </div>
     );
   }
